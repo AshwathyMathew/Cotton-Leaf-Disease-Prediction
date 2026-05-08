2cbc79bbd59b0c1a5cd711a0dda96692be8b7e33
AshwathyMathew <iamashwathy@gmail.com>
2026-05-07 21:30:18 -0400

    Add files via upload

--- Leaf_Disease_Classification_DL.py
+++ Leaf_Disease_Classification_DL.py
@@ -8,10 +8,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import tensorflow_hub as hub
-from keras.layers import Dense, Flatten, Lambda, Densem, Flatten
+from keras.layers import Dense, Flatten, Lambda, Dense, Flatten
 from keras.models import Sequential, Model
 from keras.applications.vgg16 import VGG16
-from keras.preprocessing.image import ImageDataGenerator, load_img
+from keras.src.legacy.preprocessing.image import ImageDataGenerator
+from keras.utils import load_img  # Correct import
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context  # Bypass SSL verification
 from keras.preprocessing import image
 import cv2
 import os
@@ -25,7 +28,7 @@ for layer in vgg16.layers:
 folders = glob('./Dataset/*')
 print (len(folders))
 x = Flatten()(vgg16.output)
-prediction=Dense(len(folders, activation='softmax')(x))
+prediction=Dense(len(folders), activation='softmax')(x)
 model=Model(inputs=vgg16.input, outputs=prediction)
 model.summary()
 model.compile(
@@ -36,11 +39,11 @@ model.compile(
 train_datagen = ImageDataGenerator(rescale = 1./255, shear_range =  0.2, zoom_range = 0.2, horizontal_flip = True)
 train_set = train_datagen.flow_from_directory('./Dataset', target_size = (224, 224),
                                             batch_size = 32, class_mode = 'categorical')
-r = model.fit_generator(train_set, epochs = 10, steps_per_epoch = len(train_set))
+r = model.fit(train_set, epochs = 10, steps_per_epoch = len(train_set))
 from IPython import display
 display.set_matplotlin_formats('svg')
 plt.style.use('seaborn-pastel')
-plt.plot(r,history['loss'],label='train loss')
+plt.plot(r.history['loss'],label='train loss')
 plt.legend()
 plt.show()
 plt.savefig('loss.png')
